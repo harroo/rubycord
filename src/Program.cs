5a0f7605ffaec31099c68dcd4e7a1e750216fed4
Harroo <harroo@protonmail.com>
2021-10-11 10:47:45 +1100

    start src directory with program.cs

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Rubycord {
+
+	public static class Program {
+
+		public static void Main (string[] args) {
+
+			Console.WriteLine("fuck off today world");
+		}
+	}
+}
